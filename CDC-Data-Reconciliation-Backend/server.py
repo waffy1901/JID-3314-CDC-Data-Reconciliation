03dbb37ad8ca77e6bc9460865c94628c90f4bf81
Carson Keavney <carsonkeavney@outlook.com>
2024-01-25 22:03:47 -0500

    Removed Duplicate Function and fixed app.liteConn

--- CDC-Data-Reconciliation-Backend/server.py
+++ CDC-Data-Reconciliation-Backend/server.py
@@ -162,46 +162,30 @@ async def automatic_report(year: int, cdc_file:  UploadFile = File(None)):
 
 
 @app.get("/reports/{report_id}")
-async def get_report_summary(report_id: int):
+async def get_report_cases(report_id: int):
     """
-    Endpoint to fetch a report by its ID.
+    Endpoint to fetch cases for a report by its ID.
     """
-    report = fetch_report_from_db(report_id)
+    report = fetch_reports_from_db(report_id)
     if report is None:
         raise HTTPException(status_code=404, detail="Report not found")
     return report
 
 
-def fetch_report_from_db(report_id: int):
-    """
-    Function to fetch a report from the SQLite database.
-    """
-    try:
-        database_file_path = os.path.join(os.path.dirname(__file__), "database.db")
-        liteConn = sqlite3.connect(database_file_path) 
-        cur = liteConn.cursor()
-        cur.execute("SELECT * FROM Cases WHERE reportID = ?", (report_id,))
-        report = cur.fetchone()
-        return report
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")
-        return None
-    
 def fetch_reports_from_db(report_id: int):
     """
     Function to fetch a report from the SQLite database.
     """
     try:
-        database_file_path = os.path.join(os.path.dirname(__file__), "database.db")
-        liteConn = sqlite3.connect(database_file_path) 
-        cur = liteConn.cursor()
-        cur.execute("SELECT * FROM Cases WHERE reportID = ?", (report_id,))
+        cur = app.liteConn.cursor()
+        cur.execute("SELECT * FROM Cases WHERE ReportID = ?", report_id)
         report = cur.fetchall()
         return report
     except sqlite3.Error as e:
         print(f"Database error: {e}")
         return None
-    
+
+
 def run_query(year: int):
     query = None
     query_file_path = os.path.join(app.dir, "query.sql")
