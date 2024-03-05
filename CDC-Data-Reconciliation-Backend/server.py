c6217e0804d7b97a5c7ce2f9c645bcc3f5a81458
Baute, John Nicholas <jackbaute22@gmail.com>
2024-03-04 20:37:46 -0500

    Archiving Reports functionality
    Added functionality to grab archive_path from frontend settings page, and save reports being made to it.

--- CDC-Data-Reconciliation-Backend/server.py
+++ CDC-Data-Reconciliation-Backend/server.py
@@ -10,6 +10,7 @@ import uuid
 import pyodbc
 import json
 import sqlite3
+import shutil
 
 app = FastAPI()
 
@@ -105,6 +106,12 @@ async def manual_report(state_file: UploadFile = File(None), cdc_file:  UploadFi
 
     id = str(uuid.uuid4())
     os.makedirs(os.path.join(app.dir, folder_name, id))
+    
+    # Fetching the archive_path for saving the Report
+    archive_path = await get_config_setting("archive_path")
+    # Making sure the archive_path has been set, otherwise throwing an exception
+    if not archive_path:
+        raise HTTPException(status_code=500, detail="Archive path configuration is missing")
 
     cdc_content = await cdc_file.read()
     cdc_save_to = os.path.join(app.dir, folder_name, id, cdc_file.filename)
@@ -119,6 +126,12 @@ async def manual_report(state_file: UploadFile = File(None), cdc_file:  UploadFi
     res_file = os.path.join(app.dir, folder_name, id, "results.csv")
     process = await asyncio.create_subprocess_exec(sys.executable, os.path.join(app.dir, "compare.py"), '-c', cdc_save_to, '-s', state_save_to, '-o', res_file)
     await process.wait()
+    
+    if not os.path.exists(archive_path):
+        os.makedirs(archive_path)
+    
+    # Writing the newly created results file to the archive folder too
+    shutil.copy2(res_file, archive_path)
 
     res = []
     with open(res_file, newline='') as csvfile:
@@ -170,6 +183,13 @@ async def automatic_report(year: int, cdc_file:  UploadFile = File(None)):
     if not os.path.exists(os.path.join(app.dir, folder_name)):
         os.makedirs(os.path.join(app.dir, folder_name))
 
+    # Fetching the archive_path for saving the Report
+    archive_path = await get_config_setting("archive_path")
+    # Making sure the archive_path has been set, otherwise throwing an exception
+    if not archive_path:
+        raise HTTPException(status_code=500, detail="Archive path configuration is missing")
+    
+    
     id = str(uuid.uuid4())
     os.makedirs(os.path.join(app.dir, folder_name, id))
 
@@ -190,6 +210,12 @@ async def automatic_report(year: int, cdc_file:  UploadFile = File(None)):
     res_file = os.path.join(app.dir, folder_name, id, "results.csv")
     process = await asyncio.create_subprocess_exec(sys.executable, os.path.join(app.dir, "compare.py"), '-c', cdc_save_to, '-s', state_save_to, '-o', res_file)
     await process.wait()
+
+    if not os.path.exists(archive_path):
+        os.makedirs(archive_path)
+
+    # writing the newly created results file to the archive folder too
+    shutil.copy2(res_file, archive_path)
 
     # Add results to the response
     res = []
