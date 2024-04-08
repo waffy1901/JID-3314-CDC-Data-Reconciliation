942aef3fbdb72c0bb112970335cc5ee67971a490
Carson Keavney <carsonkeavney@outlook.com>
2024-04-08 17:02:46 -0400

    Fix modal not having scrollbar when height of window is small

--- CDC-Data-Reconciliation-Frontend/src/components/Modal.jsx
+++ CDC-Data-Reconciliation-Frontend/src/components/Modal.jsx
@@ -2,10 +2,10 @@ export default function Modal({ isOpen, onClose, children }) {
   return (
     isOpen && (
       <div
-        className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50'
+        className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 overflow-auto z-50'
         onClick={onClose}
       >
-        <div onClick={(e) => e.stopPropagation()}>{children}</div>
+        <div className="max-h-screen" onClick={(e) => e.stopPropagation()}>{children}</div>
       </div>
     )
   )
