d9cbbe7ec633f41563a0e0c3c46804da657ab206
p-zach <porterdzach@gmail.com>
2024-04-08 22:27:11 -0400

    Rename modal

--- CDC-Data-Reconciliation-Frontend/src/pages/Home.jsx
+++ CDC-Data-Reconciliation-Frontend/src/pages/Home.jsx
@@ -11,6 +11,8 @@ export default function Home() {
   const [currReport, setCurrReport] = useState(null)
   const [isModalOpen, setIsModalOpen] = useState(false)
   const [deleteModalOpen, setDeleteModalOpen] = useState(false)
+  const [renameModalOpen, setRenameModalOpen] = useState(false)
+  const [renameName, setRenameName] = useState('')
   const [visibleReportsCount, setVisibleReportsCount] = useState(5);
   const [modifyingReportId, setModifyingReportId] = useState(null)
 
@@ -52,10 +54,9 @@ export default function Home() {
     if (option === "Delete") {
       setDeleteModalOpen(true)
       setModifyingReportId(id)
-      
     } else if (option === "Rename") {
-      renameReport(id)
-      fetchReportSummaries()
+      setRenameModalOpen(true)
+      setModifyingReportId(id)
     }
   }
 
@@ -73,14 +74,31 @@ export default function Home() {
   }
 
   const deleteReport = async (index) => {
-    // add a note to the "are you sure?" popup to clarify that archived CSVs are not deleted
-    const response = await fetch(config.API_URL + "/reports/" + index, {method: "DELETE"})
-
-    return response
+    return fetch(config.API_URL + "/reports/" + index, {method: "DELETE"})
   }
 
-  const renameReport = (index) => {
-    alert("Renaming reports is not implemented yet.")
+  const handleRenameReport = async (e) => {
+    setRenameModalOpen(false)
+    const newName = renameName
+    // Clear entry field
+    setRenameName('')
+    e.preventDefault();
+    const response = await renameReport(modifyingReportId, newName)
+    if (response.ok) {
+      fetchReportSummaries()
+    }
+    else {
+      console.error("Failed to rename report " + modifyingReportId)
+    }
+  }
+
+  const renameReport = async (index, newName) => {
+    alert("renaming to " + newName)
+    return { ok: true }
+  }
+
+  const handleRenameFieldChange = (e) => {
+    setRenameName(e.target.value)
   }
 
   return (
@@ -110,6 +128,37 @@ export default function Home() {
           </div>
         </div>
       </Modal>
+      <Modal isOpen={renameModalOpen} onClose={() => setRenameModalOpen(false)}>
+        <div className='bg-white rounded-md p-5'>
+          <form onSubmit={handleRenameReport}>
+            {/* TODO: Fetch the actual name of the report when it is renamed */}
+            <h2 className="text-black text-lg pb-3">
+              Enter the new name for Report {modifyingReportId}:
+            </h2>
+            <input
+              type='text'
+              id='new_name'
+              onChange={handleRenameFieldChange}
+              className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+              placeholder='Enter the new name'
+              value={renameName}
+            />
+            <div className="flex flex-row justify-center gap-10 m-2 mt-5 left-0">
+              <Button
+                text='Cancel'
+                className='px-4 py-2 shadow-lg'
+                onClick={() => setRenameModalOpen(false)}>
+              </Button>
+              <Button
+                text='Rename'
+                type='submit'
+                className='px-4 py-2 shadow-lg'
+                onClick={() => {}}>
+              </Button>
+            </div>
+          </form>
+        </div>
+      </Modal>
       <div className='flex flex-row items-center h-full w-full'>
         <div className='min-w-[300px] bg-slate-200 h-full flex flex-col items-center gap-4 p-1 pt-8 pb-8 overflow-auto'>
           <Button
@@ -134,7 +183,7 @@ export default function Home() {
                       dotsOptions.map((option) => (
                         <div 
                           key={option}
-                          onClick={(e) => {e.stopPropagation(); handleOptionClick(option, summary.ID);}} 
+                          onClick={(e) => {handleOptionClick(option, summary.ID); e.stopPropagation();}} 
                           className="text-lg font-normal hover:text-slate-500 cursor-pointer select-none">
                             {option}
                         </div>
